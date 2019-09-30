fda52ad9d508e78b4728bb7798f29d1ade18817e
Hannah Bingham <binghan.hb@gmail.com>
2019-09-30 09:01:50 -0500

    New project created to fix naming issues with original project. Project1 is now the working project. Some of the naming maybe funny but i was having an issue creating the database.

---
+++ Project1/Controllers/HerosController.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Project1.Models;
+
+namespace Project1.Controllers
+{
+    public class HerosController : Controller
+    {
+        ApplicationDbContext db;
+        public HerosController()
+        {
+            db = new ApplicationDbContext();
+        }
+        public ActionResult Index()
+        {
+            var listOfHeros = db.hero1s.ToList();
+            return View(listOfHeros);
+        }
+
+        // GET: Heros/Details/5
+        public ActionResult Details(int id)
+        {
+
+            return View();
+        }
+
+        // GET: Heros/Create
+        public ActionResult Create()
+        {
+            CreatorHero1 newHero = new CreatorHero1();
+            return View(newHero);
+        }
+
+        // POST: Heros/Create
+        [HttpPost]
+        public ActionResult Create(CreatorHero1 hero)
+
+        {
+            try
+            {
+                db.hero1s.Add(hero);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Heros/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Heros/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, CreatorHero1 hero)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Heros/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Heros/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, CreatorHero1 hero)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Project1/Models/CreatorHero1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Project1.Models
+{
+    public class CreatorHero1
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string AlterEgo { get; set; }
+        public string PrimartyAbility { get; set; }
+        public string SecondaryAbility { get; set; }
+        public string CatchPhrase { get; set; }
+
+
+    }
+}
